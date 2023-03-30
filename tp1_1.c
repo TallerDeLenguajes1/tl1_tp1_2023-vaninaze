ad47200851df78b32097330a7f85838f5cb75b8e
vaninaze <vaninazee@gmail.com>
2023-03-30 15:05:28 -0300

    punto 3 f

--- tp1_1.c
+++ tp1_1.c
@@ -1,6 +1,12 @@
 #include<stdio.h>
 int main(){
-
-    printf("Hola mundo");
+    int num=6, * punt;
+    punt = &num;
+    printf("\n Contenido del puntero: %i", *punt);
+    printf("\n Direcc de memoria que contiene el puntero: %p", punt);
+    printf("\n Direcc de memoria de la variable: %p", &num);
+    printf("\n Direcc de memoria del puntero: %p", &punt);
+    printf("\n Tamanio de memoria utilizado por num: %i", sizeof(num));
+   // printf("Hola mundo");
     return 0;
 }
